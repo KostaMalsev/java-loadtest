e9f14bd6fcde976595f155911aee1f903cc6bd24
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-09-15 12:27:02 +0300

    Update KostaClass.java

--- KostaClass.java
+++ KostaClass.java
@@ -1,2 +1,18 @@
+public class KostaClass {
 
-
+        // Instance variables (attributes)
+        private String name;
+        private long age;
+        private String city;
+        public long value;
+      
+        // Constructor to initialize the object
+        public KostaClass(long value){
+          this.name = "KOSTA";
+          this.age = value;
+          this.city = "Meitar";
+          this.value = value;
+        }
+    
+}
+
