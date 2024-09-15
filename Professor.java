c1a3dad6a8d2a3e5c2c214cf610023e2e5425c78
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-09-15 12:28:11 +0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,2 +1,15 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-
+public class Professor {
+
+    private String name;
+    private boolean tenured;
+    private List<Course> courses = new ArrayList<>();
+    private int level;
+    private LocalDate birthDay;
+    private double lastEvaluation;
+
+    // constructor
+}
